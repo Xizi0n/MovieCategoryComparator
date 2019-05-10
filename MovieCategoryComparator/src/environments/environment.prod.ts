6d6543e5803618be97d20b04254e45b3fb77da33
Nagy Ádám Richárd <nagyadam9415@gmail.com>
2019-05-10 20:53:36 +0200

    +mobile opt

--- MovieCategoryComparator/src/environments/environment.prod.ts
+++ MovieCategoryComparator/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  apiKey: '8c442117bcab888d276d170e0b16b276',
+  baseUrl: 'https://api.themoviedb.org/3/',
+  language: 'en-US',
+  baseImageUrl: 'https://image.tmdb.org/t/p/w500/'
 };
